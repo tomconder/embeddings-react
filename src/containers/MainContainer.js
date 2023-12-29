0eaae5fa0c5d40234d6345174f8cdcc7878d1773
Tom Conder <tom@itstom.co>
2023-12-29 02:20:07 -0600

    add progress

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,39 +1,110 @@
-import React from 'react';
+import {useEffect, useRef, useState} from 'react';
 import CalculateButton from '../components/CalculateButton/CalculateButton';
 import ResultsBox from '../components/ResultsBox/ResultsBox';
+import Progress from '../components/Progress';
 import styles from './MainContainer.module.css';
 
 const MainContainer = () => {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [data, setData] = React.useState([]);
+  const [ready, setReady] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const [progressItems, setProgressItems] = useState([]);
 
-  React.useEffect(() => {
-    pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2')
-        .then((response) => {
-          console.log('response', response);
-          setData(response);
-        })
-        .catch((error) => console.log(error));
-  }, []);
+  const [input1, setInput1] = useState('This is a happy person');
+  const [input2, setInput2] = useState('This is a happy person');
+  const [output, setOutput] = useState('');
 
-  React.useEffect(() => {
-    if (data) {
-      setIsLoading(false);
+  const worker = useRef(null);
+
+  useEffect(() => {
+    if (!worker.current) {
+      worker.current = new Worker(new URL('../worker.js', import.meta.url), {
+        type: 'module',
+      });
     }
-    console.log('data', data);
-  }, [data]);
+
+    const onMessageReceived = (e) => {
+      switch (e.data.status) {
+        case 'initiate':
+          // Model file start load: add a new progress item to the list.
+          setReady(false);
+          setProgressItems((prev) => [...prev, e.data]);
+          break;
+
+        case 'progress':
+          // Model file progress: update one of the progress items.
+          setProgressItems(
+              (prev) => prev.map((item) => {
+                if (item.file === e.data.file) {
+                  return {...item, progress: e.data.progress};
+                }
+                return item;
+              }),
+          );
+          break;
+
+        case 'done':
+          // Model file loaded: remove the progress item from the list.
+          setProgressItems(
+              (prev) => prev.filter((item) => item.file !== e.data.file),
+          );
+          break;
+
+        case 'ready':
+          // Pipeline ready: the worker is ready to accept messages.
+          setReady(true);
+          break;
+
+        case 'update':
+          // Generation update: update the output text.
+          setOutput(e.data.output);
+          break;
+
+        case 'complete':
+          // Generation complete: re-enable the "Translate" button
+          setDisabled(false);
+          break;
+      }
+    };
+
+    worker.current.addEventListener('message', onMessageReceived);
+
+    // Define a cleanup function for when the component is unmounted.
+    return () =>
+      worker.current.removeEventListener('message', onMessageReceived);
+  });
+
+  const calculateSimilarity = () => {
+    setDisabled(true);
+    worker.current.postMessage({
+      input1,
+      input2,
+    });
+  };
 
   return (
     <>
       <h1>Generate embeddings in the browser</h1>
 
       <div className={styles.inputs}>
-        <textarea id="input1">That is a happy person</textarea>
-        <textarea id="input2">That is a happy person</textarea>
+        <textarea id="input1" onChange={(x) => setInput1(x.target.value)}>
+        </textarea>
+        <textarea id="input2" onChange={(x) => setInput2(x.target.value)}>
+        </textarea>
       </div>
 
-      <CalculateButton disabled={isLoading} />
-      <ResultsBox></ResultsBox>
+      <CalculateButton disabled={disabled} onClick={calculateSimilarity} />
+      <ResultsBox>{ output }</ResultsBox>
+
+      <div className='progress-bars-container'>
+        {ready === false && (
+          <span>Loading models... (only run once)</span>
+        )}
+        {progressItems.map((data) => (
+          <div key={data.file}>
+            <Progress text={data.file} percentage={data.progress} />
+          </div>
+        ))}
+      </div>
 
     </>
   );
--- src/worker.js
+++ src/worker.js
@@ -5,10 +5,27 @@ class EmbeddingPipeline {
   static model = 'Xenova/all-MiniLM-L6-v2';
   static instance = null;
 
-  static async getInstance() {
+  static async getInstance(progressCallback = null) {
     if (this.instance === null) {
-      this.instance = pipeline(task, model);
+      this.instance = pipeline(task, model, {progressCallback});
     }
     return this.instance;
   }
 }
+
+// Listen for messages from the main thread
+self.addEventListener('message', async (event) => {
+  const extractor = await EmbeddingPipeline.getInstance((x) => {
+    self.postMessage(x);
+  });
+
+  const output = await extractor(event.data.text, {
+    pooling: 'mean',
+    normalize: true,
+  });
+
+  self.postMessage({
+    status: 'complete',
+    output: output,
+  });
+});
