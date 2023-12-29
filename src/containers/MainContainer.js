234005f803c4f26d88a334097ea8580551760553
Tom Conder <tom@itstom.co>
2023-12-29 03:04:02 -0600

    remove worker

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,84 +1,62 @@
-import {useEffect, useRef, useState} from 'react';
+import {useState} from 'react';
 import CalculateButton from '../components/CalculateButton/CalculateButton';
 import ResultsBox from '../components/ResultsBox/ResultsBox';
-import Progress from '../components/Progress';
 import styles from './MainContainer.module.css';
+import {pipeline} from '@xenova/transformers';
+
+class EmbeddingPipeline {
+  static task = 'feature-extraction';
+  static model = 'Xenova/all-MiniLM-L6-v2';
+  static instance = null;
+
+  static async getInstance(progressCallback = null) {
+    if (this.instance === null) {
+      this.instance =
+        pipeline(`${this.task}`, `${this.model}`, {progressCallback});
+    }
+    return this.instance;
+  }
+}
 
 const MainContainer = () => {
-  const [ready, setReady] = useState(null);
   const [disabled, setDisabled] = useState(false);
-  const [progressItems, setProgressItems] = useState([]);
 
   const [input1, setInput1] = useState('This is a happy person');
   const [input2, setInput2] = useState('This is a happy person');
   const [output, setOutput] = useState('');
 
-  const worker = useRef(null);
+  let extractor = null;
 
-  useEffect(() => {
-    if (!worker.current) {
-      worker.current = new Worker(new URL('../worker.js', import.meta.url), {
-        type: 'module',
+  const dotProduct = (a, b) => {
+    if (a.length !== b.length) {
+      throw new Error('Both arguments must have the same length');
+    }
+
+    let result = 0;
+
+    for (let i = 0; i < a.length; i++) {
+      result += a[i] * b[i];
+    }
+
+    return result;
+  };
+
+  const calculateSimilarity = async () => {
+    setDisabled(true);
+
+    if (extractor === null) {
+      extractor = await EmbeddingPipeline.getInstance((x) => {
+        self.postMessage(x);
       });
     }
 
-    const onMessageReceived = (e) => {
-      switch (e.data.status) {
-        case 'initiate':
-          // Model file start load: add a new progress item to the list.
-          setReady(false);
-          setProgressItems((prev) => [...prev, e.data]);
-          break;
+    const output1 = extractor(input1, {pooling: 'mean', normalize: true})
+        .then((response) => console.log(response));
+    const output2 = await extractor(input2, {pooling: 'mean', normalize: true});
 
-        case 'progress':
-          // Model file progress: update one of the progress items.
-          setProgressItems(
-              (prev) => prev.map((item) => {
-                if (item.file === e.data.file) {
-                  return {...item, progress: e.data.progress};
-                }
-                return item;
-              }),
-          );
-          break;
-
-        case 'done':
-          // Model file loaded: remove the progress item from the list.
-          setProgressItems(
-              (prev) => prev.filter((item) => item.file !== e.data.file),
-          );
-          break;
-
-        case 'ready':
-          // Pipeline ready: the worker is ready to accept messages.
-          setReady(true);
-          break;
-
-        case 'update':
-          // Generation update: update the output text.
-          setOutput(e.data.output);
-          break;
-
-        case 'complete':
-          // Generation complete: re-enable the "Translate" button
-          setDisabled(false);
-          break;
-      }
-    };
-
-    worker.current.addEventListener('message', onMessageReceived);
-
-    // Define a cleanup function for when the component is unmounted.
-    return () =>
-      worker.current.removeEventListener('message', onMessageReceived);
-  });
-
-  const calculateSimilarity = () => {
-    setDisabled(true);
-    worker.current.postMessage({
-      input1,
-      input2,
-    });
+    const output = dotProduct(output1.data, output2.data);
+    setOutput(output);
+    setDisabled(false);
   };
 
   return (
@@ -100,18 +78,6 @@ const MainContainer = () => {
 
       <CalculateButton disabled={disabled} onClick={calculateSimilarity} />
       <ResultsBox>{ output }</ResultsBox>
-
-      <div className='progress-bars-container'>
-        {ready === false && (
-          <span>Loading models... (only run once)</span>
-        )}
-        {progressItems.map((data) => (
-          <div key={data.file}>
-            <Progress text={data.file} percentage={data.progress} />
-          </div>
-        ))}
-      </div>
-
     </>
   );
 };
